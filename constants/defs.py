5f8c5350c12cce2b3ac8888eeb0084515040af70
babebp <tpacharawut@hotmail.com>
2023-10-23 02:40:50 +0700

    finish

---
+++ constants/defs.py
@@ -0,0 +1,3 @@
+API_KEY = "Enter your API Key"
+ACCOUNT_ID = "Enter your Account ID"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
---
+++ main.py
@@ -0,0 +1,8 @@
+from api.oanda_api import OandaApi
+from infrastructure.instrument_collection import instrumentCollection
+from infrastructure.collect_data import run_collection
+
+if __name__ == '__main__':
+    api = OandaApi()    
+    instrumentCollection.LoadInstruments("./data")
+    run_collection(instrumentCollection, api)
